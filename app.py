b9f95a7e27bd49217ef9e6de70435ade41cbc090
alex <alexanderdivagraelbangun@gmail.com>
2023-06-15 16:24:38 +0700

    update

--- app.py
+++ app.py
@@ -14,9 +14,23 @@ app = Flask(__name__)
 #     current_time = datetime.now(time_zone).strftime("%Y-%m-%d %H:%M:%S")
 #     return f"Hostname: {socket.gethostname()}<br>Current Time in Indonesia: {current_time}"    
 
+db = mysql.connector.connect(
+    host='db-uploadimage.ctnvddv68d9z.ap-northeast-3.rds.amazonaws.com',
+    user='admin',
+    password='12341234',
+    database='db-uploadimage'
+)
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/db')
+def getDB():
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM your_table')
+    results = cursor.fetchall()
+    return str(results)
 
 @app.route('/', methods = ['GET', 'POST'])
 def upload_file():
@@ -25,19 +39,6 @@ def upload_file():
       f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
       return 'file uploaded successfully'
    
-# db = mysql.connector.connect(
-#     host='db-uploadimage.ctnvddv68d9z.ap-northeast-3.rds.amazonaws.com',
-#     user='admin',
-#     password='12341234',
-#     database='db-uploadimage'
-# )
-
-# @app.route('/db')
-# def getDB():
-#     cursor = db.cursor()
-#     cursor.execute('SELECT * FROM your_table')
-#     results = cursor.fetchall()
-#     return str(results)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
