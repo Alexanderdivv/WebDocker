c5ef2823cba6d3d37caef64c0052cbdfc9f9b6a3
alex <alexanderdivagraelbangun@gmail.com>
2023-06-15 15:55:22 +0700

    initial update

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.9-slim
+
+RUN pip install flask pytz gunicorn mysql-connector-python
+
+# Allow statements and log messages to immediately appear in the Knative logs
+ENV PYTHONUNBUFFERED True
+
+# Copy local code to the container image.
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
+
+# COPY app.py /
+
+CMD ["python", "app.py"]
+
+# CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 app:app
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, url_for, request
+import os
+import mysql.connector
+from werkzeug.utils import secure_filename
+import socket
+from datetime import datetime
+import pytz
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     time_zone = pytz.timezone("Asia/Jakarta")
+#     current_time = datetime.now(time_zone).strftime("%Y-%m-%d %H:%M:%S")
+#     return f"Hostname: {socket.gethostname()}<br>Current Time in Indonesia: {current_time}"    
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route('/', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(f.filename)))
+      return 'file uploaded successfully'
+   
+# db = mysql.connector.connect(
+#     host='db-uploadimage.ctnvddv68d9z.ap-northeast-3.rds.amazonaws.com',
+#     user='admin',
+#     password='12341234',
+#     database='db-uploadimage'
+# )
+
+# @app.route('/db')
+# def getDB():
+#     cursor = db.cursor()
+#     cursor.execute('SELECT * FROM your_table')
+#     results = cursor.fetchall()
+#     return str(results)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
